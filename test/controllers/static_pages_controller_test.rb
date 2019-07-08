2699a0710be660fc444cac593f3b675a283385b6
Miyabi <miyabi.i.731@gmail.com>
2019-07-08 09:44:29 +0700

    Add Links Page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -24,6 +24,12 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
+
+  test "should get links" do
+    get contact_url
+    assert_response :success
+    assert_select "title", "Links | Ruby on Rails Tutorial Sample App"
+  end
   
   test "should get root" do
     get root_url
